7e74850e04258b79529d9f48393c74f8f80b9e4d
JYunth <jyunth28@gmail.com>
2025-03-07 02:39:52 +0530

    button implemented without contamination, but somehow still breaking

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+import type { Network } from "@aptos-labs/wallet-adapter-react";
+
+export const NETWORK: Network = 'testnet';
+export const MODULE_ADDRESS = process.env.NEXT_PUBLIC_MODULE_ADDRESS;
+export const APTOS_API_KEY = process.env.NEXT_PUBLIC_APTOS_API_KEY;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,10 +2,13 @@
 import { Card } from "@/components/ui/card";
 import { PowerSummary } from "@/components/PowerSummary";
 import { RecentTransactions } from "@/components/RecentTransactions";
+import { Head } from "react-day-picker";
+import { Header } from "@/components/Header";
 
 export default function Dashboard() {
   return (
     <div className="p-6 space-y-6">
+      <Header />
       <h1 className="text-4xl font-bold text-whitesmoke mb-8">Dashboard</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         <PowerSummary />
